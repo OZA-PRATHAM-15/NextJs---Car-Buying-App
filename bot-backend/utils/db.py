2fac498ffea054c5acd325605e2d4d070d5db038
Pratham Oza <pratham.oza10@gmail.com>
2025-07-12 19:17:44 +0530

    secret hidden

--- bot-backend/utils/db.py
+++ bot-backend/utils/db.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 def get_database():
-    connection_string = os.getenv("MONGO_URI", "mongodb+srv://prathamoza10:HhtjumLu1wIxVZdM@carbuying.qkw1h2s.mongodb.net/?retryWrites=true&w=majority&appName=carbuying")
+    connection_string = os.getenv("MONGO_URI")
+    if not connection_string:
+        raise ValueError("MONGO_URI is not set in environment variables.")
     client = MongoClient(connection_string)
     return client["test"]
